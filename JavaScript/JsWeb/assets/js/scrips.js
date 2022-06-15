106f60de824373c4ca2ccc37e952af1372cbd085
Paulo Sodré <paulohrsodre@hotmail.com>
2022-06-15 11:56:00 -0300

    Melhorias de código. Event Listener e cores

--- JavaScript/JsWeb/assets/js/scrips.js
+++ JavaScript/JsWeb/assets/js/scrips.js
@@ -1,12 +1,31 @@
-var currentNumberWrapper = document.getElementById('currentNumber');
-var currentNumber = 0;
+// var currentNumberWrapper = document.getElementById('currentNumber');
+// var currentNumber = 0;
 
-function increment() {
-    currentNumber = currentNumber + 1;
-    currentNumberWrapper.innerHTML = currentNumber;
-}
+// function increment() {
+//     currentNumber = currentNumber + 1;
+//     currentNumberWrapper.innerHTML = currentNumber;
+// }
 
-function decrement() {
-    currentNumber = currentNumber - 1;
-    currentNumberWrapper.innerHTML = currentNumber;
-}
+// function decrement() {
+//     currentNumber = currentNumber - 1;
+//     currentNumberWrapper.innerHTML = currentNumber;
+// }
+
+const btnIncrementar = document.getElementById("incrementar");
+const btnDecrementar = document.getElementById("decrementar");
+const currentNumber = document.getElementById("currentNumber");
+let contador = currentNumber.innerHTML = 0;
+
+btnIncrementar.addEventListener("click", () => {
+    currentNumber.innerHTML = ++contador;
+    if (contador >= 0) {
+        document.getElementById("currentNumber").style.color = "initial"
+    }
+})
+
+btnDecrementar.addEventListener("click", () => {
+    currentNumber.innerHTML = --contador;
+    if (contador < 0) {
+        document.getElementById("currentNumber").style.color = "red"
+    } 
+})
